94ca9fc4e717ad6b90c62f1193f14915426bd17e
sjmann-dev <sjmanndevs@gmail.com>
2024-05-16 23:43:38 +0100

    feat(ch3): add postgres connection to app

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let config = get_configuration().expect("Configuration could not be loaded");
+    let connection = PgConnection::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
