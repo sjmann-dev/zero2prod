2c6d0ec29a4591f7f0e6614a875c0fea4040a069
sjmann-dev <sjmanndevs@gmail.com>
2024-06-03 23:33:40 +0100

    feat(ch4): add default logger

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let config: zero2prod::configuration::Settings =
         get_configuration().expect("Configuration could not be loaded");
     let connection_pool = PgPool::connect(&config.database.connection_string())
--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use crate::routes;
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -9,6 +10,7 @@ pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/healthz", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             .app_data(connection.clone())
